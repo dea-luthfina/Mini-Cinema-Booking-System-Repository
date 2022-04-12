9a7ccb9671d9c55402412b9718fd64ef5f54c838
Dea Luthfina A <dealuthfina@gmail.com>
2022-04-12 22:23:20 +0700

    ticker order

---
+++ src/Core/Human.java
@@ -0,0 +1,34 @@
+package Core;
+public class Human {
+   // data
+    private String name;
+    private String phone;
+    private String amount;
+
+    // name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    // phone
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    // amount
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+}
---
+++ src/cinematickets/CinemaTickets.java
@@ -0,0 +1,11 @@
+
+package cinematickets;
+import Form.TickerOrder;
+public class CinemaTickets {
+
+    public static void main(String[] args) {
+       TickerOrder cobatabel = new TickerOrder();
+        cobatabel.setLocationRelativeTo(null);
+        cobatabel.setVisible(true);
+}
+    }
