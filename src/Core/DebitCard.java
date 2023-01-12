c9eabc8a0f5019ffc2429767609180a39597e302
Kanira Erliana Azwa Zalea <kanira.zalea@gmail.com>
2023-01-12 17:35:27 +0700

    add new java class DebitCard.java

---
+++ src/Core/DebitCard.java
@@ -0,0 +1,20 @@
+package Core;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class DebitCard extends Payment{
+    /**
+     *
+     */
+    @Override
+    public String payment() {
+    return "Debit card. Please prepare your debit card to complete the transaction.";
+  }
+}
