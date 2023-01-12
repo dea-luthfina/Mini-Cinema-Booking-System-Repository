f2c24da15893a40452f196524e5a1a246ea4a52c
Kanira Erliana Azwa Zalea <kanira.zalea@gmail.com>
2023-01-12 16:24:15 +0700

    add a java class LinkAja.java in core package

---
+++ src/Core/LinkAja.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Core;
+
+/**
+ *
+ * @author USER
+ */
+public class LinkAja extends Payment{
+    @Override
+    public String payment() {
+    return "LinkAja. Please prepare your application to complete the transaction.";
+  }
+}
